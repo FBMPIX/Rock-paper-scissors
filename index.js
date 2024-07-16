0636017d724c3841cdca713347471a086992af44
FBMPIX <165311459+FBMPIX@users.noreply.github.com>
2024-07-15 23:38:13 -0400

    Implemented Rounds

--- index.js
+++ index.js
@@ -1,36 +1,59 @@
-// Array holds the choices
-let choiceArray = ["Rock", "Paper", "Scissors"];
 
-let userPrompt = parseInt(prompt("Choices: \n1-Rock \n2-Paper \n3-Scissors \n===(Number)==="));
-let botInput = () => {
-    let randomIndex = Math.floor(Math.random() * 3);
-    return randomIndex;
-};
+const Choices = ["Rock", "Paper", "Scissors"];
 
-// Bots choice
-let botChoice = choiceArray[botInput()];
-
-// User choice
-let userChoice = choiceArray[userPrompt -1];
+let amountOfRounds = parseInt(prompt("How many rounds would you like to play?"));
 
 // Function to determine winner
 function playRound (user, bot) {
     if (user === bot) {
         return "It's a tie!";
-    } else if (
+    } else if (    
         (user === "Rock" && bot === "Scissors") ||
         (user === "Paper" && bot === "Rock") ||
-        (user === "Scissors" && bot === "Paper")
-    ) {
+        (user === "Scissors" && bot === "Paper")) 
+    {
         return "User wins!";
     } else {
         return "Bot wins!";
     }  
 }
 
-// Displaying results
-let game = playRound(userChoice, botChoice);
-alert(`
-Bot Choice: ${botChoice} \n 
-User Choice: ${userChoice} \n
-Result: ${game} `);
+let roundCounter = 0;
+let userRoundsWon = 0;
+let botRoundsWon = 0;
+let numberOfTies = 0;
+
+for (let roundCounter = 1; roundCounter <= amountOfRounds; roundCounter++) {
+    let UserInput = parseInt(
+        prompt(`
+        1-Rock 
+        2-Paper
+        3-Scissors
+        `));
+    let botIndex = Math.floor(Math.random() * 3);
+
+    let botChoice = Choices[botIndex];
+    let userChoice = Choices[UserInput -1];
+
+    let result = playRound(userChoice, botChoice);
+    
+    if (result === "User wins!") {
+        userRoundsWon++;
+    } else if (result === "Bot wins!") {
+        botRoundsWon++;
+    } else {
+        numberOfTies+=1;
+    }
+    
+    alert(`
+        ---- Round ${roundCounter} ----
+        Bot Choice: ${botChoice} 
+        User Choice: ${userChoice} 
+
+        ---- Results: ${result} ----
+        
+        Bot Rounds Won: ${botRoundsWon} 
+        User Rounds Won: ${userRoundsWon} 
+        Number of ties: ${numberOfTies} 
+    `);
+}
