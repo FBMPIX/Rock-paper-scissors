0a28060e512272648b09b18aa1282988c904e5f5
FBMPIX <165311459+FBMPIX@users.noreply.github.com>
2024-10-05 23:24:28 -0400

    made template for UI

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 
-const Choices = ["Rock", "Paper", "Scissors"];
-
-let amountOfRounds = parseInt(prompt("How many rounds would you like to play?"));
-
+const CHOICES = ["Rock", "Paper", "Scissors"];
 // Function to determine winner
 function playRound (user, bot) {
     if (user === bot) {
@@ -22,38 +19,3 @@ let roundCounter = 0;
 let userRoundsWon = 0;
 let botRoundsWon = 0;
 let numberOfTies = 0;
-
-for (let roundCounter = 1; roundCounter <= amountOfRounds; roundCounter++) {
-    let UserInput = parseInt(
-        prompt(`
-        1-Rock 
-        2-Paper
-        3-Scissors
-        `));
-    let botIndex = Math.floor(Math.random() * 3);
-
-    let botChoice = Choices[botIndex];
-    let userChoice = Choices[UserInput -1];
-
-    let result = playRound(userChoice, botChoice);
-    
-    if (result === "User wins!") {
-        userRoundsWon++;
-    } else if (result === "Bot wins!") {
-        botRoundsWon++;
-    } else {
-        numberOfTies+=1;
-    }
-    
-    alert(`
-        ---- Round ${roundCounter} ----
-        Bot Choice: ${botChoice} 
-        User Choice: ${userChoice} 
-
-        ---- Results: ${result} ----
-        
-        Bot Rounds Won: ${botRoundsWon} 
-        User Rounds Won: ${userRoundsWon} 
-        Number of ties: ${numberOfTies} 
-    `);
-}
